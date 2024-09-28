21f20bc3138e676af1caea247ffe6999a3efab56
“bhhh09” <2690757531@qq.com>
2024-09-28 18:40:28 +0800

    pcs

--- mainwindow.h
+++ mainwindow.h
@@ -104,6 +104,9 @@ public:
     CPcs* m_nRelyOnWhichCs; // 状态栏的参考坐标系
 
     //坐标系摆正
-    double AxesRotateX();
+    double AxesRotateX(); // X-Y平面的旋转角度, X轴正方向与线段之间的夹角
+    double AxesRotateY(); //
+    void on2dCoordSetRightX(); // 摆正X坐标系
+    void on2dCoordSetRightY(); // 摆正Y坐标系
 };
 #endif // MAINWINDOW_H
