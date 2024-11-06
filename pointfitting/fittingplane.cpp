1a25855559a4e9f695a8a14cb7230d2342405293
Glittering <3100497433@qq.com>
2024-11-06 16:20:48 +0800

    增加设置邻域和距离阈值的对话框

--- pointfitting/fittingplane.cpp
+++ pointfitting/fittingplane.cpp
@@ -1,8 +1,5 @@
 #include "pointfitting/fittingplane.h"
 
-#include<QWindow>
-#include<QVBoxLayout>
-
 #include<pcl/io/pcd_io.h>// PCL 的 PCD 文件输入输出类
 #include<pcl/io/ply_io.h>
 #include <pcl/point_types.h>
@@ -17,41 +14,33 @@
 FittingPlane::FittingPlane()
 {
     //创建点云智能指针
-    cloudptr.reset(new PointCloudT);
-    planeCloud.reset(new PointCloudT);
-    cloud_subset.reset(new PointCloudT);
+    // cloudptr.reset(new pcl::PointCloud<pcl::PointXYZ>);
+    // planeCloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
+    // cloud_subset.reset(new pcl::PointCloud<pcl::PointXYZ>);
 
-    //从指定路径加载 PCD 文件到点云对象中
-    pcl::io::loadPLYFile("D:/1_university/Code/Qt code/02G1_actual_cloud.ply", *cloudptr);
-    //pcl::io::loadPCDFile("D:/1_university/Code/Qt code/maize.pcd", *cloudptr);
+    // //从指定路径加载 PCD 文件到点云对象中
+    // pcl::io::loadPLYFile("D:/1_university/Code/Qt code/02G1_actual_cloud.ply", *cloudptr);
+    // //pcl::io::loadPCDFile("D:/1_university/Code/Qt code/maize.pcd", *cloudptr);
 
     coefficients.reset(new pcl::ModelCoefficients);
     inliers.reset(new pcl::PointIndices);
 
-    RANSAC();
+    //RANSAC();
 
-    // 打印平面方程的系数
-    std::cout << "Plane coefficients: " << coefficients->values[0] << " "
-              << coefficients->values[1] << " " << coefficients->values[2] << " "
-              << coefficients->values[3] << std::endl;
-
-    visualizePlane();
 }
 
-void FittingPlane::RANSAC()
+void FittingPlane::RANSAC(pcl::PointXYZ searchPoint,pcl::PointCloud<pcl::PointXYZ>::Ptr cloudptr)
 {
     //创建KD树用于邻域搜索
     pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
     kdtree.setInputCloud(cloudptr);
 
     // 初始化一个点来搜索其邻域（这里选择点云中的第一个点作为示例）
-    searchPoint = cloudptr->points[500000];
     std::vector<int> pointIdxRadiusSearch;
-    double radius = 0.1f; // 设置搜索半径
     std::vector<float> pointRadiusSquaredDistance; // 用于存储找到的点到查询点的平方距离
 
     // 搜索给定半径内的点
-    if (kdtree.radiusSearch(searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0) {
+    if (kdtree.radiusSearch(searchPoint, radious, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 3) {
 
         pcl::copyPointCloud(*cloudptr, pointIdxRadiusSearch, *cloud_subset);//在邻域点中实现RANSAC算法
 
@@ -61,7 +50,7 @@ void FittingPlane::RANSAC()
         seg.setModelType(pcl::SACMODEL_PLANE);
         seg.setMethodType(pcl::SAC_RANSAC);
         seg.setMaxIterations(1000);//设置最大迭代次数
-        seg.setDistanceThreshold(0.001);//设置阈值
+        seg.setDistanceThreshold(distance);//设置阈值
 
         // 提供输入点云
         seg.setInputCloud(cloud_subset);
@@ -80,30 +69,61 @@ void FittingPlane::RANSAC()
         planeCloud->width = planeCloud->points.size();
         planeCloud->height = 1;
         planeCloud->is_dense = true;
+
+        // 打印平面方程的系数
+        std::cout << "Plane coefficients: " << coefficients->values[0] << " "
+                  << coefficients->values[1] << " " << coefficients->values[2] << " "
+                  << coefficients->values[3] << std::endl;
+
+        //visualizePlane();
     } else {
         PCL_ERROR("Couldn't find more points within radius\n");
         return;
     }
 }
 
-bool FittingPlane::isPointInPlane(const PointT& point){
-    double distance = coefficients->values[0] * point.x +
+bool FittingPlane::isPointInPlane(const pcl::PointXYZ& point){
+    double d = coefficients->values[0] * point.x +
                       coefficients->values[1] * point.y +
                       coefficients->values[2] * point.z +
                       coefficients->values[3];
-    return std::abs(distance) <= 0.01;
+    return std::abs(d) <= 0.01;
 }
 
-void FittingPlane::visualizePlane() {
+// void FittingPlane::visualizePlane() {
 
-    pcl::visualization::PCLVisualizer viewer("3D Viewer");
+//     pcl::visualization::PCLVisualizer viewer("3D Viewer");
 
-    viewer.addPointCloud<pcl::PointXYZ>(cloudptr, "allCloud");
-    viewer.addPointCloud<pcl::PointXYZ>(planeCloud, "planeCloud");
-    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 0.0, 0.0, "planeCloud"); // 设置拟合平面内点的颜色为红色
-    viewer.setBackgroundColor(0.0, 0.0, 0.0); // 设置背景色为黑色
+//     viewer.addPointCloud<pcl::PointXYZ>(cloudptr, "allCloud");
+//     viewer.addPointCloud<pcl::PointXYZ>(planeCloud, "planeCloud");
+//     viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 0.0, 0.0, "planeCloud"); // 设置拟合平面内点的颜色为红色
+//     viewer.setBackgroundColor(0.0, 0.0, 0.0); // 设置背景色为黑色
 
-    while (!viewer.wasStopped()) {
-        viewer.spinOnce(100);
-    }
+//     while (!viewer.wasStopped()) {
+//         viewer.spinOnce(100);
+//     }
+// }
+
+void FittingPlane::setDis(){
+    dialog = new QDialog(nullptr);
+    dialog->resize(400,150);
+    layout = new QGridLayout(dialog);
+    lab1 = new QLabel("请输入邻域：");
+    lab2 = new QLabel("请输入距离阈值：");
+    rad = new QLineEdit();
+    dis = new QLineEdit();
+    btn = new QPushButton("确定");
+    layout->addWidget(lab1,0,0,1,1);
+    layout->addWidget(rad,0,1,1,2);
+    layout->addWidget(lab2,1,0,1,1);
+    layout->addWidget(dis,1,1,1,2);
+    layout->addWidget(btn,2,2,1,1);
+    dialog->setLayout(layout);
+
+    //connect(btn,&QPushButton::clicked,this,&FittingPlane::onBtnClick);
+}
+
+void FittingPlane::onBtnClick(){
+    radious=rad->text().toDouble();
+    distance=dis->text().toInt();
 }
--- pointfitting/fittingplane.h
+++ pointfitting/fittingplane.h
@@ -3,28 +3,39 @@
 
 #include <pcl/point_cloud.h>     // PCL 的点云类
 #include <pcl/visualization/pcl_visualizer.h> // PCL 的可视化工具
-
-//定义点的类型（为Vtk中的类型名创建别名）
-typedef pcl::PointXYZ PointT; // 定义 PointT 为 pcl::PointXYZ 类型
-typedef pcl::PointCloud<PointT> PointCloudT; // 定义 PointCloudT 为 pcl::PointCloud<PointT> 类型
-typedef std::shared_ptr<PointCloudT> PointCloudPtr;// 定义 PointCloudPtr 为 PointCloudT
-
-typedef pcl::visualization::PCLVisualizer PCLViewer; // 定义 PCLViewer 为 PCLVisualizer 类型
+#include <QDialog>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 class FittingPlane
 {
 public:
     FittingPlane();
-    void RANSAC();
-    bool isPointInPlane(const PointT&);
+    void RANSAC(pcl::PointXYZ,pcl::PointCloud<pcl::PointXYZ>::Ptr);
+    bool isPointInPlane(const pcl::PointXYZ&);
     void visualizePlane();
+    void setDis();//设置领域和距离阈值的对话框
+    void onBtnClick();
 private:
-    PointT searchPoint;//实现拟合平面的点
-    PointCloudPtr cloudptr;//点云智能指针
-    PointCloudPtr cloud_subset;//存储searchPoint邻域内的点
-    PointCloudPtr planeCloud;//存储拟合平面上的点
+    // pcl::PointXYZ searchPoint;//实现拟合平面的点
+    // pcl::PointCloud<pcl::PointXYZ>::Ptr cloudptr;//存储打开的点云
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_subset;//存储searchPoint邻域内的点
+    pcl::PointCloud<pcl::PointXYZ>::Ptr planeCloud;//存储拟合平面上的点
     pcl::ModelCoefficients::Ptr coefficients;//存储拟合平面方程的4个系数
     pcl::PointIndices::Ptr inliers;//存储内点
+    double radious;
+    int distance;
+
+    //对话框
+    QDialog *dialog;
+    QGridLayout *layout;
+    QLabel *lab1;
+    QLabel *lab2;
+    QLineEdit *rad;
+    QLineEdit *dis;
+    QPushButton *btn;
 };
 
 #endif // FITTINGPLANE_H
