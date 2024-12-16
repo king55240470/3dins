6b60ee398b939dad9ee6f2b568dcf6560b133459
“bhhh09” <2690757531@qq.com>
2024-12-16 21:32:33 +0800

    setDataSerialize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -257,7 +257,7 @@ void MainWindow::openFile(){
             // in.setVersion(QDataStream::Qt_6_0);
             in>>*m_EntityListMgr;
             in>>*m_ObjectListMgr;
-            // pWinFileManagerWidget->openModelFile("output.ply", "D:/output.ply");
+            pWinSetDataWidget->deserialize(in);
             qDebug() << "加载成功,m_EntityListMgr的大小为:"<<m_EntityListMgr->getEntityList().size()<<"m_ObjectListMgr的大小为:"<<m_ObjectListMgr->getObjectList().size();
             qDebug()<<"首个Object的类型为:"<<m_ObjectListMgr->GetAt(0)->GetUniqueType();
             NotifySubscribe();
@@ -306,6 +306,7 @@ void MainWindow::saveFile(){
     if(m_EntityListMgr){
         out<<*m_EntityListMgr;
         out<<*m_ObjectListMgr;
+        pWinSetDataWidget->serialize(out);
     }else{
         qWarning("Entity manager is null, nothing to save.");
     }
