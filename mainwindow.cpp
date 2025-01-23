f29581392e325c79702ec7726a3b69fb9138c29b
Glittering <3100497433@qq.com>
2025-01-23 16:58:47 +0800

    filewidget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -383,6 +383,9 @@ void MainWindow::openFile(){
             pWinToolWidget->deserializeConstructEntityList(in,pWinToolWidget->getConstructEntityList());
             pWinToolWidget->deserializeIdentifyEntityList(in,pWinToolWidget->getIdentifyEntityList());
 
+            qDebug() << "加载成功,ConstructEntityList的大小为:"<<pWinToolWidget->getConstructEntityList().size();
+            qDebug() << "加载成功,IdentifyEntityList的大小为:"<<pWinToolWidget->getIdentifyEntityList().size();
+
             qDebug() << "加载成功,m_EntityListMgr的大小为:"<<m_EntityListMgr->getEntityList().size()<<"m_ObjectListMgr的大小为:"<<m_ObjectListMgr->getObjectList().size();
             qDebug()<<"首个Object的类型为:"<<m_ObjectListMgr->GetAt(0)->GetUniqueType();
             NotifySubscribe();
