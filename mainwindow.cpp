6617bb4000b2b07774aa26ff314e380c4cc78071
unknown <2673467443@qq.com>
2025-02-27 21:00:19 +0800

    relie

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -762,6 +762,7 @@ void MainWindow::OnPresetLine(CPosition ptStart, CPosition ptEnd)
     pLine->Form="预制";
     pLine->setBegin(ptStart);
     pLine->setEnd(ptEnd);
+    pLine->setCurrentId();
     pLine->m_CreateForm = ePreset;
     pLine->m_pRefCoord = m_pcsListMgr->m_pPcsCurrent;
     pLine->m_pCurCoord = m_pcsListMgr->m_pPcsCurrent;
